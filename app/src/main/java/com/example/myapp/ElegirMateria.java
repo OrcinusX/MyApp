d5d9f80ee2f941edce9342ca07e43e3aafed8db2
OrcinusX <orcinus.comp@gmail.com>
2022-06-06 04:28:27 -0300

    Initial commit

---
+++ app/src/main/java/com/example/myapp/ElegirMateria.java
@@ -0,0 +1,61 @@
+package com.example.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class ElegirMateria extends AppCompatActivity
+        // Necesario para clickear en las cards
+        implements View.OnClickListener {
+
+    // Crear vistas de las cartas (solo hay nombres por ahora)
+    public CardView
+            cardViewMat,
+            cardViewEsi,
+            cardViewBio,
+            cardViewLen;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);                     // Default al crear la actividad
+        setContentView(R.layout.activity_elegir_materia);       // Default carga el layout
+
+        // Carga el contenido de la carta
+        // (CardView) lo convierte a vista desde el ID
+        cardViewMat = (CardView) findViewById(R.id.card_mat);
+        cardViewEsi = (CardView) findViewById(R.id.card_esi);
+        cardViewBio = (CardView) findViewById(R.id.card_bio);
+        cardViewLen = (CardView) findViewById(R.id.card_len);
+
+        // Declara que se puedan hacer click y pase algo
+        cardViewMat.setOnClickListener(this);
+        cardViewEsi.setOnClickListener(this);
+        cardViewBio.setOnClickListener(this);
+        cardViewLen.setOnClickListener(this);
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public void onClick(View v) {
+
+        // Variable intent 
+        Intent i;
+        switch (v.getId()) {
+            case R.id.card_mat:
+                i = new Intent(this, ElegirTemaSearchAndList.class);
+                startActivity(i);
+                break;
+            default:
+                Toast.makeText(ElegirMateria.this, "Proximamente...", Toast.LENGTH_SHORT).show();
+                Toast.makeText(ElegirMateria.this, "Click en: " + v.getId(), Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/myapp/ElegirTemaSearchAndList.java
@@ -0,0 +1,78 @@
+package com.example.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ElegirTemaSearchAndList extends AppCompatActivity {
+
+    public ListView temasListView;
+    public String[] txtListaMat = {"Potenciacion 1", "Potenciacion 2", "Radicacion 1", "Radicacion 2", "Fracciones", "Fracciones 2", "Fracciones 3", "Fracciones 4", "Fracciones 5"};
+    public int[] imgListaMat = {R.drawable.pot, R.drawable.pot, R.drawable.rad, R.drawable.rad, R.drawable.frc, R.drawable.frc, R.drawable.frc, R.drawable.frc, R.drawable.frc};
+    Context ctx = this;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_elegir_tema_search_and_list);
+
+        temasListView = findViewById(R.id.temas_listview);
+
+        ListELegTemaAdapter adapter = new ListELegTemaAdapter();
+        temasListView.setAdapter(adapter);
+
+        // Funcionalidad al hacer click;
+        temasListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int pos, long id) {
+                Toast.makeText(ctx, "hola", Toast.LENGTH_SHORT).show();
+                try {
+                    Thread.sleep(800);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Intent i = new Intent(ctx, MainActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+
+    public class ListELegTemaAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return txtListaMat.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int pos, View convertView, ViewGroup parent) {
+            convertView = getLayoutInflater().inflate(R.layout.card_tema, parent, false);
+            ImageView imgViewTema = convertView.findViewById(R.id.list_temas_item_img);
+            TextView txtViewTema = convertView.findViewById(R.id.list_temas_item_text);
+
+            txtViewTema.setText(txtListaMat[pos]);
+            imgViewTema.setImageResource(imgListaMat[pos]);
+
+            return convertView;
+        }
+    }
+}
